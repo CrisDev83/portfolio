83481eeed7e6145a766728c1fe089007e583d2c4
CrisDev83 <cristhian.dev83@gmail.com>
2022-08-21 20:49:58 -0300

    Atualização Desktop/Mobile

--- js/script.js
+++ js/script.js
@@ -6,3 +6,45 @@ function toggleMenu(){
 }
 
 btnMobile.addEventListener('click', toggleMenu);
+
+
+function typeWriter(elemento) {
+    const textoArray = elemento.innerHTML.split('');
+    elemento.innerHTML = '';
+    textoArray.forEach((letra, i) => {
+        setTimeout(() => {
+            elemento.innerHTML += letra;
+        }, 200 * i)
+    });
+}
+
+const title = document.querySelector('span.home-nome');
+
+typeWriter(title);
+
+
+const menuItems = document.querySelectorAll('#menu a');
+
+menuItems.forEach(item => {
+    item.addEventListener('click', scrollToIdOnClick)
+})
+
+function getScrollTopByHref(element) {
+    const id = element.getAttribute('href')
+   return document.querySelector(id).offsetTop;
+}
+
+function scrollToIdOnClick(event){
+    event.preventDefault();
+   const to = getScrollTopByHref(event.target) - 90;
+
+   scrollToPosition(to)
+}
+
+function scrollToPosition(to) {
+    window.scroll({
+        top: to,
+        behavior: "smooth"
+      })
+}
+
